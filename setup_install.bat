0c17b74fea646f853b89e957960c6bf960e4e856
gyan <gyan.gupta@vulcanaic.in>
2025-05-24 10:19:06 +0530

    Initial commit

--- setup_install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d %~dp0
-
-:: Virtual env activate (agar use karte ho)
-:: call venv\Scripts\activate.bat
-
-:: FastAPI app run karo using uvicorn
-start "" uvicorn MercuryApi:app --host 0.0.0.0 --port 8000
